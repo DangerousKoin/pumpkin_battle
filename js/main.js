569e943f4759bb5fcd3cdb61338c04cfccc519dc
kbreiten <kory.breiten@gmail.com>
2020-11-02 13:39:21 -0800

    Gameboard Down!

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 /*----- constants -----*/
+
 /*----- app's state (variables) -----*/
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+function renderPumpkin(){
+    let img = document.createElement('IMG');
+    let loc = document.getElementById('GB2');
+    img.setAttribute('src', '/home/koin/code/pumpkin_battle/images/2x2Pumpkin.png');
+    document.body.appendChild(loc);
+
+}
+
+renderPumpkin();
